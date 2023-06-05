ce3304c360c6b82a8b27114de85d78a0d99ca901
Xamrayev Xasan <xamrayevxasan11@gmail.com>
2023-06-05 13:21:07 +0500

    index.html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let alrt = alert("Hello")
+// // console.log(alrt);
+// let prmpt = prompt("ismingizni kiriting")
+// // console.log(prmpt);
+// let cnfrm = confirm("abror ismli bola maktabda o'qiydimi ?")
+// console.log(cnfrm);
+
+let a = prompt ("First number", 100)
+let b = prompt ("Second number", 20)
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
